48f9ae0b17cebc2c62cd3102cf8629f614a49e0b
Prashant Pathak <theprash@gmail.com>
2015-04-12 15:03:51 +0100

    Removed previous example code and replaced with sheetpad feature: A button to add new items.

--- src/sheetpad/core.cljs
+++ src/sheetpad/core.cljs
@@ -1,105 +1,87 @@
 (ns sheetpad.core
-  (:require-macros [reagent.ratom :refer [reaction]])  
+  (:require-macros [reagent.ratom :refer [reaction]])
   (:require [reagent.core :as reagent :refer [atom]]
             [re-frame.core :refer [register-handler
                                    path
-                                   register-sub 
-                                   dispatch 
+                                   register-sub
+                                   dispatch
                                    subscribe]]))
 
+(enable-console-print!)
 
-(defonce time-updater (js/setInterval
-                        #(dispatch [:timer (js/Date.)]) 1000))
+(defn print-db []
+  (dispatch [:print-db]))
 
-;;; this is the initial state
 (defonce initial-state
-  {:timer (js/Date.)
-   :time-color "#f34"})
+  {:sheetpad {:items []}})
+
+(defn new-editable-element [value]
+  {:value value
+   :editing false})
+
+(defn new-item [name value]
+  {:name (new-editable-element name)
+   :value (new-editable-element value)})
 
 ;; Handlers
 ;-------------------------------------------------------------
 
-;; This handler sets the initial state
 (register-handler
-  ;; the handler is passed a map (not an atom) and must return the new state
-  ;; of the db
+  :print-db
+  (fn
+    [db _]
+    (print db)
+    db))
+
+(register-handler
   :initialize
-  (fn 
+  (fn
     [db _]
     (merge db initial-state)))
 
-;; This handler changes the color of the displayed time
-(register-handler
-  ;;; register-handler can take 3 arguments to allow you to insert middleware
-  ;;; see https://github.com/Day8/re-frame/wiki/Handler-Middleware
-  :time-color
-  (path [:time-color])
-  (fn
-    ;; the path handler allows you to get directly to items in the database
-    ;; return the value you want assoc'd in
-    [time-color [_ value]]
-    value))
-
-;; This handler changes the value of the time
-(register-handler
-  :timer
-  (fn
-    ;; the first item in the second argument is :timer the second is the 
-    ;; new value
-    [db [_ value]]
-    (assoc db :timer value)))
-
-;; add subscriptions to :timer and :time-color
-(register-sub
-  :timer
-  (fn 
-    [db _]
-    ;; you need to wrap your subscription code in a reaction
-    (reaction (:timer @db))))
-
-(register-sub
-  :time-color
-  (fn 
-    [db _]
-    ;; you need to wrap your subscription code in a reaction
-    (reaction (:time-color @db))))
-
 (dispatch [:initialize])
 
+(register-handler
+  :add-item-handler
+  (path [:sheetpad :items])
+  (fn [items [handler-name value]]
+    (conj items (new-item "Unnamed Item" "No Value"))))
 
-(defn greeting [message]
-  [:h1 message])
+;; Subscriptions
+;-------------------------------------------------------------
+(register-sub
+  :items-sub
+  (fn
+    [db _]
+    (reaction (-> @db :sheetpad :items))))
 
-(defn clock []
-  (let [time-color (subscribe [:time-color])
-        timer (subscribe [:timer])]
-    ;;; wrap your component in a function to use the suscription
-    (fn [] 
-      ;; note that the initialize call will not be dispatched immediately
-      ;; as it is an async call
-      (when @timer
-        (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
-          [:div.example-clock
-           {:style {:color @time-color}}
-           time-str])))))
+;; HTML generation
+;-------------------------------------------------------------
 
-(defn color-input []
-  (let [time-color (subscribe [:time-color])]
-    ;;; wrap your component in a function to use the suscription
+(defn display-edit-control [text]
+  [:span {:on-click #(println 1)} text])
+
+(defn render-item [{n :name v :value}]
+  [:div (display-edit-control (:value n)) ": " (display-edit-control (:value v))])
+
+(defn items []
+  (let [items-sub (subscribe [:items-sub])]
     (fn []
-      [:div.color-input
-       "Time color: "
-       [:input {:type "text"
-                :value @time-color
-                :on-change #(dispatch 
-                              [:time-color (-> % .-target .-value)])}]])))
+      (into [:div.items]
+            (map render-item
+                 @items-sub)))))
 
-(defn simple-example []
+(defn add-item []
+  [:div.add-item
+   [:button
+    {:on-click #(dispatch [:add-item-handler])}
+    "Add"]])
+
+(defn app []
   [:div
-   [greeting "Hello world, it is now"]
-   [clock]
-   [color-input]])
+   [items]
+   [add-item]])
 
 (defn ^:export run []
-  (reagent/render [simple-example]
+  (reagent/render [app]
                   (js/document.getElementById "app")))
