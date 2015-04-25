aa2b984669b0f930905f3ff7d90baf4fe4e97386
Prashant Pathak <theprash@gmail.com>
2015-04-25 14:20:38 +0100

    Moved HTML generation into new namespace: render

--- src/sheetpad/core.cljs
+++ src/sheetpad/core.cljs
@@ -2,17 +2,13 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [reagent.core :as reagent :refer [atom]]
             [re-frame.core :refer [register-sub
-                                   dispatch
-                                   subscribe]]
-            [sheetpad.util :as util]
-            [sheetpad.handlers :as handlers]))
+                                   dispatch]]
+            [sheetpad.render :as render]))
 
 (enable-console-print!)
 
 (dispatch [:initialize])
 
-;; Subscriptions
-;-------------------------------------------------------------
 (register-sub
   :sheetpad-sub
   (fn
@@ -25,60 +21,4 @@
     [db _]
     (reaction (-> @db :sheetpad :items))))
 
-;; HTML generation
-;-------------------------------------------------------------
-
-(defn name-editor [item item-id]
-  (let [name (-> item :name)]
-    [:span
-     [:input {:value name
-              :on-change #(dispatch
-                            (let [new-name (-> % .-target .-value)]
-                              [:set-name item-id new-name]))}]]))
-
-(defn value-editor [item item-id]
-  (let [value (-> item :value)]
-    [:span
-     [:input {:value value
-              :on-change #(dispatch
-                            (let [new-value (-> % .-target .-value)]
-                              [:set-value item-id new-value]))}]]))
-
-(defn item [item-id item]
-  [:div.item
-   {:on-key-down (util/key-event-handler
-                   #(dispatch [:delete-item-handler item-id])
-                   util/delete-keycode)}
-   (name-editor item item-id)
-   (value-editor item item-id)
-   (when-let [calculated-value (:calculated-value item)]
-     [:span calculated-value])
-   [:span [:button
-           {:on-click #(dispatch [:delete-item-handler item-id])}
-           "x"]]])
-
-(defn items []
-  (let [items-sub (subscribe [:items-sub])]
-    (fn []
-      (into [:div.items]
-            (map-indexed item @items-sub)))))
-
-(defn add-item []
-  [:div.add-item
-   [:button
-    {:on-click #(dispatch [:add-item-handler])}
-    "Add"]])
-
-(defn db-view []
-  [:div.db-view
-   [:div (print-str @(subscribe [:sheetpad-sub]))]])
-
-(defn app []
-  [:div
-   [items]
-   [add-item]
-   [db-view]])
-
-(defn ^:export run []
-  (reagent/render [app]
-                  (js/document.getElementById "app")))
+(def ^:export run render/run)
