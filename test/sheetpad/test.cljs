d11e2ca311ed11db2ad4e45eedcd890cb8645ca3
Prashant Pathak <theprash@gmail.com>
2015-08-27 12:28:21 +0100

    Added tests

--- test/sheetpad/test.cljs
+++ test/sheetpad/test.cljs
@@ -4,14 +4,25 @@
             [sheetpad.render :as render]
             [sheetpad.calculate :as calc]))
 
-(deftest test-add
+(deftest test-parse-empty
+  (is (= [:cell [:non-formula]]
+         (calc/parse ""))))
+
+(deftest test-parse-number
+  (is (= [:cell [:non-formula [:num "123"]]]
+         (calc/parse "123"))))
+
+(deftest test-parse-formula-add
   (is (= [:cell [:formula [:num "1"] [:symbol "+"] [:num "1"]]]
-         (calc/parse "=1+1"))))
+         (calc/parse "=1+1")
+         (calc/parse "= 1 + 1 "))))
 
-(deftest test-multiply
+(deftest test-parse-formula-multiply
   (is (= [:cell [:formula [:num "1"] [:symbol "*"] [:num "1"]]]
-         (calc/parse "=1 * 1"))))
+         (calc/parse "=1*1")))
+  (is (= [:cell [:formula [:num "3"] [:symbol "*"] [:num "4"]]]
+         (calc/parse "=3 * 4"))))
 
-(deftest test-item
+(deftest test-parse-formula-item
   (is (= [:cell [:formula [:item "a"]]]
          (calc/parse "=[a]"))))
