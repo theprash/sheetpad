a310b79f5ec75c034925b40055e265066708303c
Prashant Pathak <theprash@gmail.com>
2015-08-28 13:59:49 +0100

    Tests for calculating plain number

---
+++ test/sheetpad/calculate-tests.cljs
@@ -0,0 +1,10 @@
+(ns sheetpad.calculate-tests
+  (:require [cemerick.cljs.test :refer-macros [is are deftest testing use-fixtures done]]
+            [sheetpad.calculate :as calc]))
+
+(deftest test-number
+  (is (= 1
+         (calc/parse-and-calculate "1")
+         (calc/parse-and-calculate " 1 ")
+         (calc/parse-and-calculate "=1")
+         (calc/parse-and-calculate "= 1 "))))
